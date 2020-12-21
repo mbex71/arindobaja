d61b57669d4b0034436f3a3952dfa7d8c27d9d0d
Muhammad Firdaus <mbex71@gmail.com>
2020-12-21 08:23:23 +0700

    last updated

--- resources/assets/js/components/Fbchat.js
+++ resources/assets/js/components/Fbchat.js
@@ -2,33 +2,56 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
 
 
-  <div className="fb-customerchat"
-    attribution="setup_tool"
-    page_id="195262631045929">
-  </div>
+//   <div className="fb-customerchat"
+//     attribution="setup_tool"
+//     page_id="195262631045929">
+//   </div>
 
 
 
 class Fbchat extends Component{
+    constructor(props){
+        super(props);
+        this._kirimpesan = this._kirimpesan.bind(this);
+    }
+
+    _kirimpesan(){
+        window.open('https://wa.me/628119327889','__blank');
+    }
     render(){
         
         return(
-            <div>
-            <div className="fb-customerchat"
-                attribution="setup_tool"
-                page_id="195262631045929">
+            <div className="container-fluid fixed-bottom">
+                
+                
+                <button className="btn btn-success float-right btn-lg my-3 shadow" onClick={this._kirimpesan}>
+                <i className="fab fa-whatsapp mr-3"></i>
+                Chat Or Call</button>
+                
             </div>
-            </div>
+
+            // <div>
+            // <div className="fb-customerchat"
+            //     attribution="setup_tool"
+            //     page_id="195262631045929">
+            // </div>
+            // </div>
         )
+    }
+}
+
+const style = {
+    roundbutton:{
+
     }
 }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>PT Arindo Baja Cipta</title>
 
@@ -12,6 +13,7 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
        
     </head>
     <body>
